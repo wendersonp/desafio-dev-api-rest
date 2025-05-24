3ab2f3fcf48a2cf93c917d195a1af64897e9422a
wendersonp <wendersonpsg21@gmail.com>
2025-05-24 01:00:07 -0300

    Configuração da pipeline de deploy e cluster ECS

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -2,11 +2,53 @@ resource "aws_iam_group" "ecr_group" {
   name = "ecr-group"
 }
 
+resource "aws_iam_policy" "ecs_ecr_custom_policy" {
+  name        = "ECSECRGithubDeployPolicy"
+  description = "Custom policy for ECS and ECR access for Github Actions"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecs:DescribeTaskDefinition",
+          "ecs:UpdateService"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_group_policy_attachment" "policy_attachment" {
   for_each = toset(var.ecr_policy_arn)  
 
   group = aws_iam_group.ecr_group.name
   policy_arn = each.value
+}
+
+resource "aws_iam_group_policy_attachment" "attach_ecs_ecr_policy" {
+  group = aws_iam_group.ecr_group.name
+  policy_arn = aws_iam_policy.ecs_ecr_custom_policy.arn
 }
 
 resource "aws_iam_user" "ecr_user" {
